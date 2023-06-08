4d35dd865d4f52f5a169cb8616049e259cd6dfd5
Terrence Newton <terrence.newton@icloud.com>
2023-06-07 21:27:08 -0400

    Added display property to enum Gender, with constructor, default values, and toString

--- backend/src/main/java/com/terrence/customer/Gender.java
+++ backend/src/main/java/com/terrence/customer/Gender.java
@@ -1,6 +1,19 @@
 package com.terrence.customer;
 
 public enum Gender {
-    MALE,
-    FEMALE
+    MALE ("Male"),
+    FEMALE ("Female");
+
+    private final String display;
+
+    Gender(String display) {
+        this.display = display;
+    }
+
+    @Override
+    public String toString() {
+        return "Gender{" +
+                "display='" + display + '\'' +
+                '}';
+    }
 }
