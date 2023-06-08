1c10c7d011de28f71f0ae5053dcfd0f0c95b3535
Terrence Newton <terrence.newton@icloud.com>
2023-06-07 23:11:52 -0400

    Fixing gender in CustomerRowMapper

--- backend/src/main/java/com/terrence/customer/CustomerRowMapper.java
+++ backend/src/main/java/com/terrence/customer/CustomerRowMapper.java
@@ -15,7 +15,7 @@ public class CustomerRowMapper implements RowMapper<Customer> {
                 rs.getString("name"),
                 rs.getString("email"),
                 rs.getInt("age"),
-                Gender.fromText(rs.getString("gender"))
+                Gender.valueOf(rs.getString("gender"))
         );
     }
 }
