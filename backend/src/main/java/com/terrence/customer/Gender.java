279b286192cb0c240243fab81da43233e35b7d73
Terrence Newton <terrence.newton@icloud.com>
2023-06-08 00:02:15 -0400

    Changed property names to all caps

--- backend/src/main/java/com/terrence/customer/Gender.java
+++ backend/src/main/java/com/terrence/customer/Gender.java
@@ -1,6 +1,6 @@
 package com.terrence.customer;
 
 public class Gender {
-    public static final String female = "Female";
-    public static final String male = "Male";
+    public static final String FEMALE = "Female";
+    public static final String MALE = "Male";
 }
