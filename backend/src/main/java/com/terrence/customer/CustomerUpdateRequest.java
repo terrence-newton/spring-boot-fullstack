fa9f9cab8212da804e3dce54dd1fbd131b55e801
Terrence Newton <terrence.newton@icloud.com>
2023-06-07 21:57:31 -0400

    Changed CustomerUpdateRequest.gender from String to Gender

--- backend/src/main/java/com/terrence/customer/CustomerUpdateRequest.java
+++ backend/src/main/java/com/terrence/customer/CustomerUpdateRequest.java
@@ -6,6 +6,6 @@ public record CustomerUpdateRequest(
         Optional<String> name,
         Optional<String> email,
         Optional<Integer> age,
-        Optional<String> gender
+        Optional<Gender> gender
 ) {
 }
