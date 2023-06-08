99f534b90bcaf9c235200559a6d416636113a694
Terrence Newton <terrence.newton@icloud.com>
2023-06-07 21:23:34 -0400

    In Customer, changed type of all gender references to enum Gender. Added @Enumerated(EnumType.STRING) annotation to gender. Condensed @Column annotations.

--- backend/src/main/java/com/terrence/customer/Customer.java
+++ backend/src/main/java/com/terrence/customer/Customer.java
@@ -15,7 +15,7 @@ import java.util.Objects;
         }
 )
 public class Customer {
-    public Customer(String name, String email, Integer age, String gender) {
+    public Customer(String name, String email, Integer age, Gender gender) {
         this.name = name;
         this.email = email;
         this.age = age;
@@ -33,24 +33,17 @@ public class Customer {
             generator = "customer_id_seq"
     )
     private Integer id;
-    @Column(
-            nullable = false
-    )
+    @Column(nullable = false)
     private String name;
-    @Column(
-            nullable = false
-    )
+    @Column(nullable = false)
     private String email;
-    @Column(
-            nullable = false
-    )
+    @Column(nullable = false)
     private Integer age;
-    @Column(
-            nullable = false
-    )
-    private String gender;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Gender gender;
 
-    public Customer(Integer id, String name, String email, Integer age, String gender) {
+    public Customer(Integer id, String name, String email, Integer age, Gender gender) {
         this.id = id;
         this.name = name;
         this.email = email;
@@ -108,7 +101,7 @@ public class Customer {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Customer customer = (Customer) o;
-        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age) && Objects.equals(gender, customer.gender);
+        return Objects.equals(id, customer.id) && Objects.equals(name, customer.name) && Objects.equals(email, customer.email) && Objects.equals(age, customer.age) && gender == customer.gender;
     }
 
     @Override
@@ -117,10 +110,10 @@ public class Customer {
     }
 
     public String getGender() {
-        return gender;
+        return gender.toString();
     }
 
-    public void setGender(String gender) {
+    public void setGender(Gender gender) {
         this.gender = gender;
     }
 }
