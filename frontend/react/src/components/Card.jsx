ae7ce3b1468798bc4d0f0ddd648b31f458ba5127
Terrence Newton <terrence.newton@icloud.com>
2023-06-08 00:35:42 -0400

    Changed photo in Card to random photo based on gender

--- frontend/react/src/components/Card.jsx
+++ frontend/react/src/components/Card.jsx
@@ -33,7 +33,9 @@ export default function CardWithImage({id, name, email, age, gender}) {
                     <Avatar
                         size={'xl'}
                         src={
-                            'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&ixid=eyJhcHBfaWQiOjE3Nzg0fQ'
+                            'https://randomuser.me/api/portraits/'
+                            + (gender == 'Male' ? 'men' : 'women')
+                            + '/' + Math.floor(Math.random() * 100) + '.jpg'
                         }
                         alt={'Author'}
                         css={{
