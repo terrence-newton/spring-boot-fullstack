a9cb6131cb4c4fc0c471e1600d877e2f9d5431ad
Terrence Newton <terrence.newton@icloud.com>
2023-06-07 21:58:13 -0400

    Added gender to new Customer in CustomerService.addCustomer

--- backend/src/main/java/com/terrence/customer/CustomerService.java
+++ backend/src/main/java/com/terrence/customer/CustomerService.java
@@ -32,7 +32,8 @@ public class CustomerService {
                     new Customer(
                             customerRegistrationRequest.name(),
                             customerRegistrationRequest.email(),
-                            customerRegistrationRequest.age()
+                            customerRegistrationRequest.age(),
+                            customerRegistrationRequest.gender()
                     )
             );
         } else {
